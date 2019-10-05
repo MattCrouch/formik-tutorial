bcb609adbfacb7ac25047bc30f1940cd4a2abcfd
Matt Crouch <matt@mattcrouch.net>
2019-10-05 18:03:48 +0100

    Validation

--- src/Vote.js
+++ src/Vote.js
@@ -1,19 +1,52 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
+import classNames from "classnames";
 import "./Vote.css";
 
 // Form component
-const Form = ({ handleSubmit, handleChange, handleBlur, values, errors }) => (
+const Form = ({ errors, handleSubmit }) => (
   <form className="vote" onSubmit={handleSubmit}>
-    <label htmlFor="name">Name</label>
-    <input autoComplete="name" type="text" id="name" name="name" />
+    <div className="input-group">
+      <label htmlFor="name">Name</label>
+      <div
+        className={classNames({
+          "validation-group": true,
+          error: !!errors.name
+        })}
+      >
+        <Field autoComplete="name" type="text" id="name" name="name" />
+        {!!errors.name && <div className="error-message">{errors.name}</div>}
+      </div>
+    </div>
+
     <input type="submit" value="Vote now" />
   </form>
 );
 
 // Exported function
 function Vote() {
-  return <Formik render={props => <Form {...props} />} />;
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  const validate = values => {
+    const errors = {};
+
+    if (!values.name) {
+      errors.name = "Name is required";
+    }
+
+    return errors;
+  };
+
+  return (
+    <Formik
+      initialValues={{ name: "" }}
+      onSubmit={onSubmit}
+      validate={validate}
+      render={props => <Form {...props} />}
+    />
+  );
 }
 
 export default Vote;
