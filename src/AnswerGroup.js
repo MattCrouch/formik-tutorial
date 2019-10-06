a6f6d9ad9b293fe1a9de744bf5037dd5042ac847
Matt Crouch <matt@mattcrouch.net>
2019-10-06 18:31:35 +0100

    Answer validation

--- src/AnswerGroup.js
+++ src/AnswerGroup.js
@@ -1,22 +1,34 @@
 import React from "react";
+import classNames from "classnames";
 import "./AnswerGroup.css";
 
 function AnswerGroup({ options, field, form, props }) {
   return (
     <fieldset className="answer-group">
       <legend>Vote</legend>
-      {options.map(({ label, value }) => (
-        <React.Fragment key={value}>
-          <input
-            {...field}
-            id={`answer-${value}`}
-            type="radio"
-            name={field.name}
-            value={value}
-          />
-          <label htmlFor={`answer-${value}`}>{label}</label>
-        </React.Fragment>
-      ))}
+      <div
+        className={classNames({
+          "validation-group": true,
+          error: !!form.errors[field.name] && form.touched[field.name]
+        })}
+      >
+        {options.map(({ label, value }) => (
+          <React.Fragment key={value}>
+            <input
+              {...field}
+              id={`answer-${value}`}
+              type="radio"
+              name={field.name}
+              value={value}
+            />
+            <label htmlFor={`answer-${value}`}>{label}</label>
+          </React.Fragment>
+        ))}
+
+        {!!form.errors[field.name] && form.touched[field.name] && (
+          <div className="error-message">{form.errors[field.name]}</div>
+        )}
+      </div>
     </fieldset>
   );
 }
