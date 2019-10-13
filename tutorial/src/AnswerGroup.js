5a5d77f12c3d33a037aaacc1a4ed1eb1ec9a084e
Matt Crouch <matt@mattcrouch.net>
2019-10-13 20:15:41 +0100

    Provide `AnswerGroup` by default

--- tutorial/src/AnswerGroup.js
+++ tutorial/src/AnswerGroup.js
@@ -4,7 +4,37 @@ import "./AnswerGroup.css";
 
 // List different options to select one from
 function AnswerGroup({ field, form, options }) {
-  return <fieldset className="answer-group"></fieldset>;
+  return (
+    <fieldset className="answer-group">
+      <legend>Answer</legend>
+      {/* Apply a class if there is an error */}
+      <div
+        className={classNames({
+          "validation-group": true,
+          error: !!form.errors[field.name] && form.touched[field.name]
+        })}
+      >
+        {/* Iterate over each option and create a `radio` input for it */}
+        {options.map(({ label, value }) => (
+          <React.Fragment key={value}>
+            <input
+              {...field}
+              id={`answer-${value}`}
+              name={field.name}
+              type="radio"
+              value={value}
+            />
+            <label htmlFor={`answer-${value}`}>{label}</label>
+          </React.Fragment>
+        ))}
+
+        {/* Conditionally show an error if the field has been touched */}
+        {!!form.errors[field.name] && form.touched[field.name] && (
+          <div className="error-message">{form.errors[field.name]}</div>
+        )}
+      </div>
+    </fieldset>
+  );
 }
 
 export default AnswerGroup;
